5c00337d35a110c7d625e24e70687d66132b69cd
zoneme99 <jonatancervall@gmail.com>
2024-10-23 12:50:36 +0200

    made progress in pandas excer.

--- Sandbox/test.py
+++ Sandbox/test.py
@@ -1,13 +1,6 @@
 import pandas as pd
 
-man = pd.read_csv("Data/man.csv")
-kvinna = pd.read_csv("Data/kvinnor.csv")
+df = pd.DataFrame({'A':[1, 2, 3, 4, 5], 'B':[6, 7, 8, 9, 10], 'C':[11, 12, 13, 14, 15]})
 
-man.rename(columns={"Antal Män 2020": "Antal 2020", "Antal Män 2019": "Antal 2019"}, inplace=True)
-kvinna.rename(columns={"Antal kvinnor 2020": "Antal 2020", "Antal kvinnor 2019": "Antal 2019"}, inplace=True)
-kvinna.rename(columns={"Förändring (%)": "Förändring"}, inplace=True)
-
-
-df = pd.concat([man, kvinna], ignore_index=True)
-df.set_index("Kommun", inplace=True)
+print(df.loc[:,'B':])
 
