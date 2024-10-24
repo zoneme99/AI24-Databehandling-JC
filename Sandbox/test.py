99347fad76f87fb6f13a5e381062bfb0d135b214
Jonatan Cervall <jonatancervall@gmail.com>
2024-10-24 15:04:24 +0200

    done with g)

--- Sandbox/test.py
+++ Sandbox/test.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
-df = pd.DataFrame({'A':[1, 2, 3, 4, 5], 'B':[6, 7, 8, 9, 10], 'C':[11, 12, 13, 14, 15]})
+df = pd.DataFrame({'A':["hej", "på", "dig", "ok",'no'], 'B':[6, 7, 8, 9, 10], 'C':[11, 12, 13, 14, 15]})
+df1 = pd.DataFrame({'A':["hej", "på", "dig", "ok",'no'], 'B':[11, 12, 13, 14, 15], 'C':[11, 12, 13, 14, 15]})
 
-print(df.loc[:,'B':])
-
+new = pd.concat([df,df1])
+result = new.groupby('A').apply(lambda x: (x['B'] - x['B']) / (x['B'] + x['B']))
+#TODO Above function doesnt work, I want the duplicate of B to make operation with first B
+print(result)
