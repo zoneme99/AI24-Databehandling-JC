cff989db71d7919b2de3415295493f8a8eb5ab66
zoneme99 <jonatancervall@gmail.com>
2024-10-29 13:06:58 +0100

    klar med lab1

--- Sandbox/test.py
+++ Sandbox/test.py
@@ -4,11 +4,9 @@ from IPython.display import display
 import plotly_express as px
 
 
-# titanic = sns.load_dataset("titanic")
-# group = titanic.groupby(["class","sex"])[['age','fare']].mean()
-# display(group)
-# display(titanic.head())
-
-lst = [1,2,3]
-
-print(True if "p" in {"Hej","på","dig"} else False)
+titanic = sns.load_dataset("titanic")
+group = titanic.groupby(["class","sex"])[['age','fare']].mean()
+group1 = titanic.groupby("class")[['age','fare']].mean()
+display(group)
+display(group1)
+display(titanic.head())
