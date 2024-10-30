0fc9691d466f4fff424e614bf19a4af0f3b95d69
zoneme99 <jonatancervall@gmail.com>
2024-10-30 12:44:47 +0100

    continue 1g E01

--- Exercises/data_utils.py
+++ Exercises/data_utils.py
@@ -5,3 +5,13 @@ def plt_nans(df):
     nan_counts = df.isna().sum()
     nan_counts = nan_counts[nan_counts > 0]  # Keep only columns with NaNs
     nan_counts.plot(kind='bar', title='Number of NaNs per Column', color=plt.cm.tab20(range(len(nan_counts))))
+
+def type_mask(df):
+	mask = pd.DataFrame()
+	for col in df.columns:
+		mask[col] = df[col].apply(lambda x: type(x))
+	return mask
+
+def uniques(df):
+	for col in df.columns:
+		print(f"{col} : {df[col].unique()}")
