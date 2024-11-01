6234eb9b415c6ce3ca0c77267812655c36e357c1
Jonatan Cervall <jonatancervall@gmail.com>
2024-11-01 13:47:02 +0100

    continue 2g

--- Exercises/data_utils.py
+++ Exercises/data_utils.py
@@ -5,7 +5,7 @@ import seaborn as sns
 def plt_nans(df):
 	nan_counts = df.isna().sum()
 	nan_counts = nan_counts[nan_counts > 0]
-	sns.barplot(x=nan_counts.index, y=nan_counts.values, hue=nan_counts.index, palette=sns.color_palette("blend:#00D9FF,#D2FF61", n_colors=len(nan_counts.index)), alpha=0.7)
+	sns.barplot(x=nan_counts.index, y=nan_counts.values, hue=nan_counts.index, palette=my_palette(len(nan_counts.index)), alpha=0.7)
 	plt.show()
 
 def type_mask(df):
@@ -18,10 +18,6 @@ def uniques(df):
 	for col in df.columns:
 		print(f"{col} : {df[col].unique()}")
 
-def sns_theme(hue_length):
-	'''
-	returns my personal palette and alpha
-	hue_length is the number of unique values in defined hue argument for seaborn (integer)
-	'''
-	alpha = 0.7
-	return alpha, sns.color_palette("blend:#00D9FF,#D2FF61", n_colors=hue_length)
+def my_palette(size):
+	return sns.color_palette("blend:#00D9FF,#D2FF61", n_colors=size)
+
