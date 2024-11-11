7cfe3fee1d35a689d0fdda2e60366bfc320b8112
Jonatan Cervall <jonatancervall@gmail.com>
2024-11-11 15:01:26 +0100

    app works as expected

--- Dash/app.py
+++ Dash/app.py
@@ -9,17 +9,28 @@ sinus = px.line(x= X, y= np.sin(X))
 cosinus = px.line(x= X, y= np.cos(X))
 
 app.layout = html.Div(className='parent', children=[
-    html.Div(className='child',children=dcc.Graph(figure=sinus)),
-    html.Div(className='child',children=dcc.Graph(figure=cosinus))
+    html.Div(className='child',children=dcc.Graph(figure={}, id="sin")),
+    html.Div(className='child',children=dcc.Graph(figure=cosinus, id="cos"))
     ]
-    html.Div(className='row', children=[
-    dcc.RadioItems(options=[10, 20, 50, 100],
-                    value= 10,
+), html.Div(className='row', children=[
+    dcc.RadioItems(options=[*range(5, 55, 5)],
+                    value= 5,
                     inline=True,
                     id='my-radio-buttons-final')
     ]
     )
+
+@callback(
+    [Output(component_id='sin', component_property='figure'),
+     Output(component_id='cos', component_property='figure')],
+    Input(component_id='my-radio-buttons-final', component_property='value')
 )
+
+def update_graphs(samples_chosen):
+    X = np.linspace(0, 10, samples_chosen)
+    sinus = px.line(x= X, y= np.sin(X))
+    cosinus = px.line(x= X, y= np.cos(X))
+    return sinus, cosinus
 
 
 
