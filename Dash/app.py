0b5f3536228702357c762bec24f0c4ee4eaff42d
zoneme99 <jonatancervall@gmail.com>
2024-11-14 10:43:34 +0100

    bootstrap row and cols works

--- Dash/app.py
+++ Dash/app.py
@@ -2,23 +2,29 @@ from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
 import numpy as np
+import dash_bootstrap_components as dbc
 
-app = Dash()
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 X =np.linspace(0, 10, 100)
 sinus = px.line(x= X, y= np.sin(X))
 cosinus = px.line(x= X, y= np.cos(X))
 
-app.layout = html.Div(className='parent', children=[
-    html.Div(className='child',children=dcc.Graph(figure={}, id="sin")),
-    html.Div(className='child',children=dcc.Graph(figure=cosinus, id="cos"))
-    ]
-), html.Div(className='row', children=[
-    dcc.RadioItems(options=[*range(5, 55, 5)],
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(dcc.Graph(figure={}, id="sin")),
+        dbc.Col(dcc.Graph(figure=cosinus, id="cos"))])
+    
+    ,
+    html.Div(children=[
+        dcc.RadioItems(options=[*range(5, 55, 5)],
                     value= 5,
                     inline=True,
                     id='my-radio-buttons-final')
     ]
     )
+    ], fluid=True
+    
+)
 
 @callback(
     Output(component_id='sin', component_property='figure'),
